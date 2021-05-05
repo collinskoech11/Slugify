3357baad763db50b5e4583e66ab2e1c5bcf57900
collinskoech11 <collinskoechck34@gmail.com>
2021-05-05 21:19:45 +0300

    update

--- project/urls.py
+++ project/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from  slugify.views import slugify
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', slugify, name="slugify"),
 ]
