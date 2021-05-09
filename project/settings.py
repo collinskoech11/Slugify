bf051184ba8664b3aa8550beec55576722b2d70d
collinskoech11 <collinskoechck34@gmail.com>
2021-05-09 16:17:22 +0300

    update

--- project/settings.py
+++ project/settings.py
@@ -121,6 +121,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATICFILES = [BASE_DIR / "static"]
+STATIC_ROOT = "static_root"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
